2e3b92d449528d7e2372a2a0a8331f83ff0454a8
iqramalik85 <140602763+iqramalik85@users.noreply.github.com>
2024-02-12 21:11:21 +0500

    Update ndjson_script.sh

--- ndjson_script.sh
+++ ndjson_script.sh
@@ -1,6 +1,12 @@
-
 #!/bin/bash
-file=$1
+
+filename=$1
+
+# Check if the file exists
+if [ ! -f "$filename" ]; then
+    echo "File $filename not found."
+    exit 1
+fi
 
 echo -n "Enter the replacement word for 'Product_name': "
 read -r product
@@ -8,21 +14,25 @@ echo -n "Enter the replacement word for 'Endpoint_name' with 1st alphabet capita
 read -r endpoint
 echo -n "Enter the replacement word for 'Request_uri': "
 read -r request_uri
-echo -n "Enter the replacement word for 'Default-index-pattern': "
-read -r def_index
 echo -n "Enter the replacement word for 'Index-pattern': "
 read -r index
-mapfile -t client_urls < domain_file
-for ((i=0; i<${#client_urls[@]}; i++)); do
-    if grep -q "DOMAIN$((i+1))" "$file"; then
-        sed -i "s/DOMAIN$((i+1))/${client_urls[i]}/g" "$file"
+
+mapfile -t client_urls < url_file
+
+# Loop through the client_urls and use sed to perform the replacements in the file
+for ((i=1; i<${#client_urls[@]}; i++)); do
+    # Check if the word "DOMAIN" with the current index exists in the file before using sed
+    if grep -q "DOMAIN$i\b" "$filename"; then
+        sed -i "s/DOMAIN$i\b/${client_urls[i]}/g" "$filename"
     else
-        echo "Error: 'DOMAIN$((i+1))' not found in $file."
+        echo "Error: 'DOMAIN$i' not found in $filename."
     fi
 done
-sed -i "s/PRODUCT/$product/g" "$file"
-sed -i "s/ENDPOINT/$endpoint/g" "$file"
-sed -i "s/REQ_URI/$request_uri/g" "$file"
-sed -i "s/DEF_INDEX/$def_index/g" "$file"
-sed -i "s/INDEX/$index/g" "$file"
-echo "Replacements complete in $file."
+
+# Perform additional replacements based on user input
+sed -i "s/PRODUCT/$product/g" "$filename"
+sed -i "s/ENDPOINT/$endpoint/g" "$filename"
+sed -i "s/REQ_URI/$request_uri/g" "$filename"
+sed -i "s/INDEX/$index/g" "$filename"
+
+echo "Replacements complete in $filename."
