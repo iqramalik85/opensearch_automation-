2509e2f35e5cd7cffaf7aaaf60b32f10c0996608
iqramalik85 <140602763+iqramalik85@users.noreply.github.com>
2024-02-26 17:23:07 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -21,13 +21,4 @@ sed -i "s/ENDPOINT/$endpoint/g" "$filename"
 sed -i "s/REQ_URI$request_uri/g" "$filename"
 sed -i "s/INDEX/$index/g" "$filename"
 
-git config --global user.email "malikayra85@gmail.com"
-git config --global user.name "iqramalik85"
-git init
-git remote add origin git@github.com/iqramalik85/opensearch_automation-.git
-git branch -M main
-git add .
-git commit -m 'feat: My first feature'
-git push -u origin main
-
 echo "Replacements complete in $filename. Changes pushed to GitHub."
