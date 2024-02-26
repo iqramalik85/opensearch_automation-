f9a3635c76d633c2051dc20ca3f6774b5f35b5f2
iqramalik85 <140602763+iqramalik85@users.noreply.github.com>
2024-02-26 17:23:31 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -18,7 +18,7 @@ done
 
 sed -i "s/PRODUCT/$product/g" "$filename"
 sed -i "s/ENDPOINT/$endpoint/g" "$filename"
-sed -i "s/REQ_URI$request_uri/g" "$filename"
+sed -i "s/REQ_URI/$request_uri/g" "$filename"
 sed -i "s/INDEX/$index/g" "$filename"
 
 echo "Replacements complete in $filename. Changes pushed to GitHub."
